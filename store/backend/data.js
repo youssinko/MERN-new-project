60563e35eac52cd6a33628dccfe1c8421c34ded6
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-12-23 23:49:24 -0500

    added backend

---
+++ store/backend/data.js
@@ -0,0 +1,31 @@
+const data ={
+    products: [
+        {
+        name:'Tumbler',
+        slug:'color-changing tumbler',
+        category: 'Tumblers',
+        image: '/images/tumbler.jpeg',
+        price: 15,
+        numReviews:10,
+        description: "personalized color-changing tumbler",
+    },
+    {
+        name:'Shirts',
+        slug:'long-sleeve shirts',
+        category: 'shirts',
+        image: '/images/tshirt.jpeg',
+        price: 30,
+        numReviews:10,
+        description: "personalized long-sleeve shirts",
+    },   {
+        name:'stickers',
+        slug:'clear foil sticker',
+        category: 'stickers',
+        image: '/images/sticker.jpeg',
+        price: 30,
+        numReviews:40,
+        description: "personalized foil clear sticker, qty:100",
+    },
+]
+}
+export default data
