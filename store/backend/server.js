94cc304f34a025282c1497330070885de56b08e8
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-12-26 20:44:30 -0500

    connected mongoDB

--- store/backend/server.js
+++ store/backend/server.js
@@ -1,27 +1,41 @@
-import express from 'express';
+import express from "express";
 import data from "./data.js";
-const app = express()
-app.get('/api/products', (req,res)=>{
-    //send back data to front end
-    res.send(data.products)
-})
-app.get('/api/products/slug/:slug', (req,res)=>{
-    const product = data.products.find((x)=> x.slug === req.params.slug)
-    if (product){
-    res.send(product)
-    }else{
-        res.status(404).send({message:'Product not found'})
-    }
-})
-app.get('/api/products/:id', (req,res)=>{
-    const product = data.products.find((x)=> x._id === req.params.id)
-    if (product){
-    res.send(product)
-    }else{
-        res.status(404).send({message:'Product not found'})
-    }
-})
-const port=process.env.PORT || 5000;
-app.listen(port,()=>{
-    console.log(`on port ${port}`)
-})
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+//to fetch variables in the env file
+dotenv.config();
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to Mongo db");
+  })
+  .catch((err) => {
+    console.Console(err.message);
+  });
+
+const app = express();
+app.get("/api/products", (req, res) => {
+  //send back data to front end
+  res.send(data.products);
+});
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`on port ${port}`);
+});
